1f0d83a8f82da0c480f26fe4e25be97616f41408
PAULINE MOULLE <pauline.moulle@etu.univ-lyon1.fr>
2020-10-26 11:02:02 +0100

    essai modeliser les contraintes

--- find_sub_graph.py
+++ find_sub_graph.py
@@ -8,16 +8,33 @@ import numpy
 
 #node = Variable(graph.keys())
 def get_model(N, n,proba, clues):
-    matrice=Matrix(N, N, 0,(N*proba))
+    graph=graph_alea_number(N,proba)
+    matrice=Matrix(graph)
     #print(matrice)
     sub_graph=graph_alea_number(n,1/2)
     sub_matrice=Matrix(sub_graph)
-    model=Model([(x == int(v)) for x, v in zip(matrice.flat, "".join(open(clues)).split())],
-                [matrice[x:x+n,y:y+n] == sub_graph
-                for x in range(0,N,1)
-                for y in range (0,N,1)],
-                )
-    return(matrice,model)
+    """
+    list_sum=[]
+    for i in range(n):
+        sum=0
+        for j in range(n):
+            sum+=sub_graph[i][j]
+        list_sum.append(sum)
+    """    
+    
+    solv=Matrix(n+1,n+1,0,N) # matrice resultat with ligne 0 et col 0 == node big graph,
+    model=Model(solv[0][0] == -1,
+                [AllDiff(row) for row in solv.row]) #all node is different
+    for i in range(1,n+1):
+        model.add(solv[0][i]==solv[i][0]) #node ligne 0 et col 0 equal
+    for i in range(1,n):
+        for j in range(2,n+1):  # Contraite des arcs , pas de contrainte en col 1 et ligne n+1
+            model.add(matrice[solv[0][j]][solv[i][0]] == sub_matrice[i-1][j-1])
+        
+
+    #toujours pas bon il faut remplir solv avec les .add            
+
+    return(solv,model)
 
 def solve(param):
     N = param['N']
@@ -27,7 +44,7 @@ def solve(param):
     write_matrix_file(graph)
     clues=param['file']
 
-    matrice, model = get_model(N, n ,proba, clues)
+    solv, model = get_model(N, n ,proba, clues)
 
     solver = model.load(param['solver'])
     solver.setVerbosity(param['verbose'])
@@ -36,7 +53,7 @@ def solve(param):
     solver.solve()
     out = ""
     if solver.is_sat():
-        out = True
+        out = solv
     else:
         out = False
 
